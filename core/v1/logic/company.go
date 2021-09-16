044423135d4efc666570a77c39d534d8dd895caa
zeromsi <www.zeromsi2@gmail.com>
2021-09-16 11:59:16 +0600

    close #3 , add company service

---
+++ core/v1/logic/company.go
@@ -0,0 +1,50 @@
+package logic
+
+import (
+	v1 "github.com/klovercloud-ci/core/v1"
+	"github.com/klovercloud-ci/core/v1/repository"
+	"github.com/klovercloud-ci/core/v1/service"
+	"github.com/klovercloud-ci/enums"
+)
+
+type companyService struct {
+	repo repository.CompanyRepository
+}
+
+func (c companyService) Store(company v1.Company) error {
+	panic("implement me")
+}
+
+func (c companyService) Update(company v1.Company, companyUpdateOption ...v1.CompanyUpdateOption) {
+	panic("implement me")
+}
+
+func (c companyService) Delete(companyId string) error {
+	panic("implement me")
+}
+
+func (c companyService) GetCompanies(option v1.CompanyQueryOption) []v1.Company {
+	panic("implement me")
+}
+
+func (c companyService) GetByCompanyId(id string, option v1.CompanyQueryOption) v1.Company {
+	panic("implement me")
+}
+
+func (c companyService) GetRepositoriesByCompanyId(id string, option v1.CompanyQueryOption) []v1.Repository {
+	panic("implement me")
+}
+
+func (c companyService) GetApplicationsByCompanyId(id string, option v1.CompanyQueryOption) []v1.Application {
+	panic("implement me")
+}
+
+func (c companyService) GetApplicationsByCompanyIdAndRepositoryType(id string, _type enums.REPOSITORY_TYPE, option v1.CompanyQueryOption) []v1.Application {
+	panic("implement me")
+}
+
+func NewCompanyService(repo repository.CompanyRepository) service.Company {
+	return &companyService{
+		repo: repo,
+	}
+}
---
+++ core/v1/service/company.go
@@ -0,0 +1,17 @@
+package service
+
+import (
+	v1 "github.com/klovercloud-ci/core/v1"
+	"github.com/klovercloud-ci/enums"
+)
+
+type Company interface {
+	Store(company v1.Company) error
+	Update(company v1.Company,companyUpdateOption ... v1.CompanyUpdateOption)
+	Delete(companyId string) error
+	GetCompanies(option v1.CompanyQueryOption) []v1.Company
+	GetByCompanyId(id string,option v1.CompanyQueryOption) v1.Company
+	GetRepositoriesByCompanyId(id string,option v1.CompanyQueryOption) []v1.Repository
+	GetApplicationsByCompanyId(id string,option v1.CompanyQueryOption) []v1.Application
+	GetApplicationsByCompanyIdAndRepositoryType(id string,_type enums.REPOSITORY_TYPE,option v1.CompanyQueryOption) []v1.Application
+}
