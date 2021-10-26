0a678899d4e08d223c022bbf53191fb15c78c939
zeromsi <zeromsi.official@gmail.com>
2021-10-26 17:46:01 +0600

    refactor company logic

--- core/v1/logic/company.go
+++ core/v1/logic/company.go
@@ -17,12 +17,7 @@ func (c companyService) GetRepositoryByRepositoryId(id string) v1.Repository {
 }
 
 func (c companyService) GetApplicationByCompanyIdAndRepositoryIdAndApplicationUrl(companyId, repositoryId, applicationUrl string) v1.Application {
-	return v1.Application{
-		MetaData: v1.ApplicationMetadata{
-			Id: "1001",
-		},
-	}
-	//return c.repo.GetApplicationByCompanyIdAndRepositoryIdAndApplicationUrl(companyId, repositoryId, applicationUrl)
+	return c.repo.GetApplicationByCompanyIdAndRepositoryIdAndApplicationUrl(companyId, repositoryId, applicationUrl)
 }
 func (c companyService) UpdateRepositories(company v1.Company, companyUpdateOption v1.CompanyUpdateOption) error {
 	if companyUpdateOption.Option == enums.APPEND_REPOSITORY {
@@ -72,11 +67,7 @@ func (c companyService) UpdateApplications(companyId string, repositoryId string
 }
 
 func (c companyService) GetRepositoryByCompanyIdAndApplicationUrl(id, url string) v1.Repository {
-	return v1.Repository{
-		Id:    "1",
-		Token: "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",
-	}
-	//return c.repo.GetRepositoryByCompanyIdAndApplicationUrl(id, url)
+	return c.repo.GetRepositoryByCompanyIdAndApplicationUrl(id, url)
 }
 
 func (c companyService) GetCompanyByApplicationUrl(url string) v1.Company {
@@ -101,27 +92,19 @@ func (c companyService) GetCompanies(option v1.CompanyQueryOption) []v1.Company 
 }
 
 func (c companyService) GetByCompanyId(id string, option v1.CompanyQueryOption) (v1.Company, int64) {
-	company, total := c.repo.GetByCompanyId(id, option)
-
-	return company, total
+	return c.repo.GetByCompanyId(id, option)
 }
 
 func (c companyService) GetRepositoriesByCompanyId(id string, option v1.CompanyQueryOption) ([]v1.Repository, int64) {
-	repositories, total := c.repo.GetRepositoriesByCompanyId(id, option)
-
-	return repositories, total
+	return c.repo.GetRepositoriesByCompanyId(id, option)
 }
 
 func (c companyService) GetApplicationsByCompanyId(id string, option v1.CompanyQueryOption) ([]v1.Application, int64) {
-	applications, total := c.repo.GetApplicationsByCompanyId(id, option)
-
-	return applications, total
+	return  c.repo.GetApplicationsByCompanyId(id, option)
 }
 
 func (c companyService) GetApplicationsByCompanyIdAndRepositoryType(id string, _type enums.REPOSITORY_TYPE, option v1.CompanyQueryOption) []v1.Application {
-	applications := c.repo.GetApplicationsByCompanyIdAndRepositoryType(id, _type, option)
-
-	return applications
+	return c.repo.GetApplicationsByCompanyIdAndRepositoryType(id, _type, option)
 }
 
 func NewCompanyService(repo repository.CompanyRepository) service.Company {
