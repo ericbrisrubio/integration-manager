306b9711ee2ec6e176ea078a35ad98f1e98b3e1b
zeromsi <zeromsi.official@gmail.com>
2021-10-31 15:43:12 +0600

    refactor

--- core/v1/logic/company.go
+++ core/v1/logic/company.go
@@ -49,6 +49,9 @@ func (c companyService) UpdateRepositories(company v1.Company, companyUpdateOpti
 func getUsernameAndRepoNameFromGithubRepositoryUrl(url string) (username string, repoName string) {
 	trim := strings.TrimSuffix(url, ".git")
 	urlArray := strings.Split(trim, "/")
+	if len(urlArray)<3{
+		return "",""
+	}
 	repositoryName := urlArray[len(urlArray)-1]
 	usernameOrorgName := urlArray[len(urlArray)-2]
 	return usernameOrorgName, repositoryName
