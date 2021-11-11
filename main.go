838567f8404947e0208ff725727c5e8181c2fd07
zeromsi <zeromsi.official@gmail.com>
2021-11-11 16:40:51 +0600

    regenerate swagger

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"github.com/klovercloud-ci-cd/integration-manager/api"
 	"github.com/klovercloud-ci-cd/integration-manager/config"
+	_ "github.com/klovercloud-ci-cd/integration-manager/docs"
 )
 
-// @title Klovercloud-ci-integration-manager API
-// @description Klovercloud-ci-integration-manager API
+// @title integration-manager API
+// @description integration-manager API
 func main() {
 	e := config.New()
 	api.Routes(e)
