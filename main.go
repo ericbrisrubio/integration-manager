c4b243d749c6194a22fb8ae84403541cce531683
shabrul <shabrul2451@gmail.com>
2022-03-30 13:01:12 +0600

    removed create company in runtime

--- main.go
+++ main.go
@@ -3,34 +3,13 @@ package main
 import (
 	"github.com/klovercloud-ci-cd/integration-manager/api"
 	"github.com/klovercloud-ci-cd/integration-manager/config"
-	v1 "github.com/klovercloud-ci-cd/integration-manager/core/v1"
-	"github.com/klovercloud-ci-cd/integration-manager/dependency"
 	_ "github.com/klovercloud-ci-cd/integration-manager/docs"
-	"github.com/klovercloud-ci-cd/integration-manager/enums"
 )
 
 // @title integration-manager API
 // @description integration-manager API
 func main() {
 	e := config.New()
-	initCompany()
 	api.Routes(e)
 	e.Logger.Fatal(e.Start(":" + config.ServerPort))
-}
-
-func initCompany() {
-	companyService := dependency.GetV1CompanyService()
-	err := companyService.Store(v1.Company{
-		MetaData: v1.CompanyMetadata(struct {
-			Labels                    map[string]string
-			NumberOfConcurrentProcess int64
-			TotalProcessPerDay        int64
-		}{Labels: nil, NumberOfConcurrentProcess: 10, TotalProcessPerDay: 10}),
-		Id:     config.CompanyId,
-		Name:   config.CompanyName,
-		Status: enums.ACTIVE,
-	})
-	if err != nil {
-		return
-	}
 }
